2c7e4b886bb1b5415528bb15ca3cde2c2366681b
Matthew Allison <matthewallisonxyz@gmail.com>
2024-05-07 17:17:14 +0200

    Create 2877. Create a DataFrame from List.py

---
+++ 2877. Create a DataFrame from List.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
+    student_data_frame = pd.DataFrame(student_data, columns=['student_id', 'age'])
+    return student_data_frame
+
