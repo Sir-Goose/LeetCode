f80638161633248e6030d7ce8070cb76ecefc4e6
Matthew Allison <matthewallisonxyz@gmail.com>
2024-05-12 17:22:52 +0200

    Create 2553. Separate the Digits in an Array.py

---
+++ 2553. Separate the Digits in an Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def separateDigits(self, nums: List[int]) -> List[int]:
+        output = []
+        for num in nums:
+            for digit in str(num):
+                output.append(int(digit))
+        return output
+
