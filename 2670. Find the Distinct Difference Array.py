3a4a445b49c7512d711c585751ca135b8adbd14a
Matthew Allison <matthewallisonxyz@gmail.com>
2024-05-16 16:16:08 +0200

    Create 2670. Find the Distinct Difference Array.py

---
+++ 2670. Find the Distinct Difference Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def distinctDifferenceArray(self, nums: List[int]) -> List[int]:
+        diff = [0] * len(nums)
+        for i in range(len(nums)):
+            diff[i] = len(set(nums[0:i+1])) - len(set(nums[i+1:]))
+        return diff
+
