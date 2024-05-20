763621e43df66d7f3fa4bc3c0b2036771832f3ff
Matthew Allison <matthewallisonxyz@gmail.com>
2024-05-20 21:42:18 +0200

    Create 933. Number of Recent Calls.go

---
+++ 933. Number of Recent Calls.go
@@ -0,0 +1,25 @@
+type RecentCounter struct {
+    requests []int
+}
+
+
+func Constructor() RecentCounter {
+    return RecentCounter{requests: []int{}}
+}
+
+
+func (this *RecentCounter) Ping(t int) int {
+    this.requests = append(this.requests, t)
+    for len(this.requests) > 0 && this.requests[0] < t-3000 {
+        this.requests = this.requests[1:]
+    }
+    
+    return len(this.requests)
+}
+
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Ping(t);
+ */
